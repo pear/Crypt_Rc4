fbb15c496690e4fabcc683a61e13e63f32b59874
Dave Mertens <zyprexia@php.net>
2003-10-04 16:39:32 +0000

    Reformatted the file to conform the pear CS (spaces, brackets, etc). No functional changes.
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_Rc4/trunk@141816 c90b9560-bf6c-de11-be94-00142212c4b1

--- Rc4.php
+++ Rc4.php
@@ -40,31 +40,31 @@
 * @access public
 * @package Crypt
 * @author Dave Mertens <dmertens@zyprexia.com>
- */       
+ */
 class Crypt_RC4 {
-    
+
     /**
     * Real programmers...
     * @var array
     */
-	var $s= array();
+    var $s= array();
     /**
     * Real programmers...
     * @var array
     */
-	var $i= 0;
+    var $i= 0;
     /**
     * Real programmers...
     * @var array
-    */	
-	var $j= 0;
-	
-	/**
-	* Key holder
-	* @var string
-	*/
-	var $_key;
-	
+    */
+    var $j= 0;
+
+    /**
+    * Key holder
+    * @var string
+    */
+    var $_key;
+
     /**
     * Constructor
     * Pass encryption key to key()
@@ -75,18 +75,16 @@ class Crypt_RC4 {
     * @access public
     */
     function Crypt_RC4($key = null) {
-        if ($key != null)
-        {
-        	$this->setKey($key);
+        if ($key != null) {
+            $this->setKey($key);
         }
     }
-    
-    function setKey($key)
-    {
-    	if (strlen($key) > 0)
-    		$this->_key = $key;
+
+    function setKey($key) {
+        if (strlen($key) > 0)
+            $this->_key = $key;
     }
-    
+
     /**
     * Assign encryption key to class
     *
@@ -94,21 +92,21 @@ class Crypt_RC4 {
     * @return void
     * @access public    
     */
-	function key(&$key) {
-		$len= strlen($key);
-		for ($this->i = 0; $this->i < 256; $this->i++) {
-			$this->s[$this->i] = $this->i;
-		}
+    function key(&$key) {
+        $len= strlen($key);
+        for ($this->i = 0; $this->i < 256; $this->i++) {
+            $this->s[$this->i] = $this->i;
+        }
 
-		$this->j = 0;
-		for ($this->i = 0; $this->i < 256; $this->i++) {
-			$this->j = ($this->j + $this->s[$this->i] + ord($key[$this->i % $len])) % 256;
-			$t = $this->s[$this->i];
-			$this->s[$this->i] = $this->s[$this->j];
-			$this->s[$this->j] = $t;
-		}
-		$this->i = $this->j = 0;
-	}
+        $this->j = 0;
+        for ($this->i = 0; $this->i < 256; $this->i++) {
+            $this->j = ($this->j + $this->s[$this->i] + ord($key[$this->i % $len])) % 256;
+            $t = $this->s[$this->i];
+            $this->s[$this->i] = $this->s[$this->j];
+            $this->s[$this->j] = $t;
+        }
+        $this->i = $this->j = 0;
+    }
 
     /**
     * Encrypt function
@@ -117,24 +115,24 @@ class Crypt_RC4 {
     * @return void
     * @access public    
     */
-	function crypt(&$paramstr) {
+    function crypt(&$paramstr) {
 
-		//Init key for every call, Bugfix 22316
-		$this->key($this->_key);
+        //Init key for every call, Bugfix 22316
+        $this->key($this->_key);
 
-		$len= strlen($paramstr);
-		for ($c= 0; $c < $len; $c++) {
-			$this->i = ($this->i + 1) % 256;
-			$this->j = ($this->j + $this->s[$this->i]) % 256;
-			$t = $this->s[$this->i];
-			$this->s[$this->i] = $this->s[$this->j];
-			$this->s[$this->j] = $t;
+        $len= strlen($paramstr);
+        for ($c= 0; $c < $len; $c++) {
+            $this->i = ($this->i + 1) % 256;
+            $this->j = ($this->j + $this->s[$this->i]) % 256;
+            $t = $this->s[$this->i];
+            $this->s[$this->i] = $this->s[$this->j];
+            $this->s[$this->j] = $t;
 
-			$t = ($this->s[$this->i] + $this->s[$this->j]) % 256;
+            $t = ($this->s[$this->i] + $this->s[$this->j]) % 256;
 
-			$paramstr[$c] = chr(ord($paramstr[$c]) ^ $this->s[$t]);
-		}
-	}
+            $paramstr[$c] = chr(ord($paramstr[$c]) ^ $this->s[$t]);
+        }
+    }
 
     /**
     * Decrypt function
@@ -143,11 +141,11 @@ class Crypt_RC4 {
     * @return void
     * @access public    
     */
-	function decrypt(&$paramstr) {
-		//Decrypt is exactly the same as encrypting the string. Reuse (en)crypt code
-		$this->crypt($paramstr);
-	}
-	
+    function decrypt(&$paramstr) {
+        //Decrypt is exactly the same as encrypting the string. Reuse (en)crypt code
+        $this->crypt($paramstr);
+    }
+
 
 }	//end of RC4 class
 ?>
