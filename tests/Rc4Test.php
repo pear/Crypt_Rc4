598f9c5466d4d59074cb34780e2fdeab8bbbeacf
Ken Guest <kguest@php.net>
2009-02-09 00:25:51 +0000

    Bug #15885 unit tests not working - old version of phpunit
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_Rc4/trunk@275379 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/Rc4Test.php
+++ tests/Rc4Test.php
@@ -1,20 +1,24 @@
 <?php
 
-require_once( "PHPUnit2/Framework/TestCase.php" );
+require_once( "PHPUnit/Framework/TestCase.php" );
 require_once( "Crypt/Rc4.php");
 
+if (!defined("PHPUnit_MAIN_METHOD")) {
+    define("PHPUnit_MAIN_METHOD", "Rc4Test::main");
+}
+
 //Unit test for PHP5 version of RC4
-class Rc4Test extends PHPUnit2_Framework_TestCase {
+class Rc4Test extends PHPUnit_Framework_TestCase {
 	private $_key = "PEAR";
 	private $_message = "PEAR Rulez!";
-	
+
 	public function testSimpleEncryption()
 	{
 		$rc4 = new Crypt_Rc4;
 		$rc4->key($this->_key);
 		$this->assertEquals('4kwQ6uYzPplnt0Q=', base64_encode($rc4->encrypt($this->_message)));
 	}
-	
+
 	//Test simple decryption. Checking result
 	public function testSimpleDecryption()
 	{
@@ -22,7 +26,7 @@ class Rc4Test extends PHPUnit2_Framework_TestCase {
 		$rc4->key($this->_key);
 		$this->assertEquals('PEAR Rulez!', $rc4->decrypt(base64_decode('4kwQ6uYzPplnt0Q=')));
 	}
-	
+
 	//Test encryption and than decryption of the encrypted string. The result must be the same as the message at the start
 	//This test is based on the usage example in the Rc4 class. It also test fix for PHP issue #22316
 	public function testRoundRobinEncryption()
@@ -33,7 +37,7 @@ class Rc4Test extends PHPUnit2_Framework_TestCase {
 		$message = $rc4->decrypt($message);
 		$this->assertEquals($this->_message, $message);
 	}
-	
+
 	//test RC4 assigning key using the constructor
 	public function testConstuctor()
 	{
